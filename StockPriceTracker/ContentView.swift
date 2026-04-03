5243ee9ecb995ed33a27e2cf3df4d6e78cbe6912
youssef esmail <youssef.esmailelfeky@gmail.com>
2026-04-03 17:29:19 +0200

    Initial Commit

---
+++ StockPriceTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StockPriceTracker
+//
+//  Created by Mader's Macbook Pro on 03/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StockPriceTracker/StockPriceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockPriceTrackerApp.swift
+//  StockPriceTracker
+//
+//  Created by Mader's Macbook Pro on 03/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct StockPriceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
